896794a4aaa9c4ca70e84bcb87b079e743e87bc2
simon <simon.hobbs@electrooptical.net>
2025-09-02 17:24:01 -0400

    combining the shared hal

--- renode-bootloader/project/bootloader/include/hal/logging.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-void example_log(const char *fmt, ...);
-
-#define EXAMPLE_LOG(...)                            \
-  do {                                              \
-    example_log(__VA_ARGS__);                       \
-  } while (0)
--- renode-bootloader/project/bootloader/include/shell_port.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <stdbool.h>
-
-//! Get's the next available char on the input stream
-bool shell_port_getchar(char *c_out);
-
-__attribute__((noreturn))
-void shell_processing_loop(void);
